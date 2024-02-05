7f417831b3bc897bdbbbd2c5d14dba8c42675b68
TanyaCST <schen@smith.edu>
2024-02-05 15:54:53 -0500

    Rubric Level 1 and 2 Done

--- computer.py
+++ computer.py
@@ -1,10 +1,19 @@
 class Computer:
 
     # What attributes will it need?
+    description: str
+    operating_system: str
+    
 
     # How will you set up your constructor?
+    # Type self in the parenthesis and assign the parameters info and os into description and operating_system.
+
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, info, os):
+        self.description = info # You'll remove this when you fill out your constructor
+        self.operating_system = os
 
     # What methods will you need?
+    # Chaning the Operating System of computer.
+    def refurbish_os(self, os):
+        self.operating_system = os
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,26 @@
 class ResaleShop:
 
     # What attributes will it need?
+    inventory = [] #Computer objects will go over there
+    price: float
+    
+
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, computer_obj, original_price):
+        self.inventory = computer_obj # You'll remove this when you fill out your constructor
+        self.price = original_price
 
     # What methods will you need?
+    # Buy a conputer --- add an inventory
+    def add_inventory(self, computer_obj):
+        self.inventory.append(computer_obj)
+
+    # Sell a computer --- remove an inventory
+    def remove_inventory(self, computer_obj):
+        self.inventory.remove(computer_obj)
+
+    # Change price of computers
+    def price_change(self, changed_price):
+        self.price = changed_price
